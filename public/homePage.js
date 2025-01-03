f64190ea1201adbb29e2a73fd52b92055f4c3494
Ice2508 <Ice2508@yandex.ru>
2025-01-03 15:20:19 +0300

    решение курсовой

---
+++ public/homePage.js
@@ -0,0 +1,144 @@
+
+
+
+"use strict";
+
+//выход из личного кабинета
+
+const logoutButton = new LogoutButton();
+
+logoutButton.action = function() {
+  ApiConnector.logout((responseBody) => {
+    if (responseBody.success) {
+      location.reload();
+    } else {
+      console.error("Ошибка деавторизации:", responseBody);
+    }
+  });
+};
+
+ApiConnector.current((data) => {
+  if (data.success) {
+    ProfileWidget.showProfile(data.data);
+  } else {
+    console.error('Ошибка при получении данных текущего пользователя:', data.error);
+  }
+});
+
+//Отображение курса валют
+
+const ratesBoard = new RatesBoard();
+
+function handleStocksData(response) {
+  if (response.success) {
+    // Очищаем таблицу
+    ratesBoard.clearTable();
+    ratesBoard.fillTable(response.data);
+  } else {
+    console.error("Ошибка получения курсов валют");
+  }
+}
+
+function updateRates() {
+  ApiConnector.getStocks((data) => {
+    if (data.success) {
+      ratesBoard.clearTable();
+      ratesBoard.fillTable(data.data);
+    } else {
+      console.error("Ошибка при получении курсов валют: ", data.error);
+    }
+  });
+}
+
+updateRates();
+
+setInterval(updateRates, 60000);
+
+//пополнение баланса
+
+const moneyManager = new MoneyManager();
+
+moneyManager.addMoneyCallback = (data) => {
+  ApiConnector.addMoney(data, (response) => {
+    if (response.success) {
+      ApiConnector.current((profileResponse) => {
+        if (profileResponse.success) {
+          showProfile(profileResponse.data);
+        }
+      });
+      moneyManager.setMessage(true, "Баланс успешно пополнен.");
+    } else {
+      moneyManager.setMessage(false, response.error);
+    }
+  });
+};
+
+//конвертация валют
+
+moneyManager.conversionMoneyCallback = (data) => {
+  ApiConnector.convertMoney(data, (response) => {
+    if (response.success) {
+      ApiConnector.current((profileResponse) => {
+        if (profileResponse.success) {
+          showProfile(profileResponse.response);
+        }
+      });
+      moneyManager.setMessage(true, "Конвертация успешно выполнена.");
+    } else {
+      moneyManager.setMessage(false, response.error);
+    }
+  });
+};
+
+// перевод валют
+
+moneyManager.sendMoneyCallback = (data) => {
+  ApiConnector.transferMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(true, "Перевод выполнен успешно!");
+    } else {
+      moneyManager.setMessage(false, `Ошибка перевода: ${response.error}`);
+    }
+  });
+};
+
+//избранное 
+
+const favoritesWidget = new FavoritesWidget();
+
+ApiConnector.getFavorites((response) => {
+  if (response.success) {
+    favoritesWidget.clearTable();
+    favoritesWidget.fillTable(response.data);
+    moneyManager.updateUsersList(response.data);
+  } else {
+    favoritesWidget.setMessage(false, response.error);
+  }
+});
+
+favoritesWidget.addUserCallback = (data) => {
+  ApiConnector.addUserToFavorites(data, (response) => {
+    if (response.success) {
+      favoritesWidget.setMessage(true, 'Пользователь успешно добавлен в избранное');
+      favoritesWidget.clearTable();
+      favoritesWidget.fillTable(response.data);
+      moneyManager.updateUsersList(response.data);
+    } else {
+      favoritesWidget.setMessage(false, response.error);
+    }
+  });
+};
+
+favoritesWidget.removeUserCallback = (userId) => {
+  ApiConnector.removeUserFromFavorites(userId, (response) => {
+    if (response.success) {
+      favoritesWidget.setMessage(true, 'Пользователь успешно удалён из избранного');
+      favoritesWidget.clearTable();
+      favoritesWidget.fillTable(response.data);
+      moneyManager.updateUsersList(response.data);
+    } else {
+      favoritesWidget.setMessage(false, response.error);
+    }
+  });
+};
